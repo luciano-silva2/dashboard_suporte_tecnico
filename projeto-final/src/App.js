415628685da867d69fed7a8229c8fbd880d90185
AntonyRNS <antony.ryan.n.s@gmail.com>
2025-07-14 14:18:42 +0000

    v1.3(Sidebar, Header buttons)

--- projeto-final/src/App.js
+++ projeto-final/src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header/Header'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Home from "./Pages/Home/Home";
+
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
+      <Sidebar />
+      <div style={{ marginLeft: '200px', padding: '2rem' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/filtros" element={<Home />} />
+          <Route path="/configuracoes" element={<Home />} />
+        </Routes>
+      </div>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- projeto-final/src/Components/Header/Header.jsx
+++ projeto-final/src/Components/Header/Header.jsx
@@ -1,7 +1,15 @@
+import BotaoPerfil from '../Buttons/BotaoPerfil/BotaoPerfil';
+import BotaoSair from '../Buttons/BotaoSair/BotaoSair';
+
 export default function Header() {
     return (
-        <div className="border border-dark bg-secondary text-white p-3 text-center">
-            Header
+        <div className="d-flex justify-content-between align-items-center bg-secondary text-white border border-dark p-3">
+            <h5 className="m-0">Header</h5>
+
+            <div className="d-flex align-items-center">
+                <BotaoPerfil />
+                <BotaoSair />
+            </div>
         </div>
     );
 }
