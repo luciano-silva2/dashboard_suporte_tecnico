6414be8cd52cc3d62cf5f14db6ac73be806a2ff4
AntonyRNS <antony.ryan.n.s@gmail.com>
2025-07-14 02:42:42 +0000

    v1.1(Header)

---
+++ projeto-final/src/Components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <div style={{ border: '2px solid black', padding: '10px' }}>
+            Footer
+        </div>
+    )
+}
---
+++ projeto-final/src/Components/Header/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <div style={{ border: '2px solid black', padding: '10px' }}>
+            Header
+        </div>
+    )
+}
