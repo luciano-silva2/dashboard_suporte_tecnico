e9edd1d294c6f51f0a8b21f04416ff98db7461f8
AntonyRNS <antony.ryan.n.s@gmail.com>
2025-07-25 04:42:10 +0000

    v2.0(Tema escuro/claro)

--- projeto-final/src/Components/Footer/Footer.jsx
+++ projeto-final/src/Components/Footer/Footer.jsx
@@ -1,6 +1,13 @@
+import { useTheme } from "../Context/ContextoTema";
 export default function Footer() {
+    const { theme } = useTheme();
+
+    const bgClass = theme === "light" ? "bg-light" : "bg-dark";
+    const textClass = theme === "light" ? "text-dark" : "text-white";
+    const borderClass = theme === "light" ? "border-dark" : "border-light";
+
     return (
-        <div className="border border-dark bg-secondary text-white text-center p-3 fixed-bottom">
+        <div className={`text-center p-3 fixed-bottom ${bgClass} ${textClass} ${borderClass} border`}>
             Footer
         </div>
     );
