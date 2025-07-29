4a8ba8e9b250ec954e4594bdeef54fb34c080ec7
GabrielGreapy <159476031+GabrielGreapy@users.noreply.github.com>
2025-07-29 01:25:39 +0000

    Login no inicio agora

--- projeto-final/src/App.js
+++ projeto-final/src/App.js
@@ -4,24 +4,35 @@ import Sidebar from './Components/Sidebar/Sidebar';
 import Home from "./Pages/Home/Home";
 import Tickets from "./Pages/Tickets/Tickets";
 import Chat from './Chat/Chat';
+import Login from './Login/Login';
 import { Routes, Route } from "react-router-dom";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './Firebase/firebase';
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <>
-      <Header />
-      <Sidebar />
-      <div style={{ marginLeft: '200px', padding: '2rem' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/filtros" element={<Home />} />
-          <Route path="/tickets" element={<Tickets />} />
-          <Route path="/configuracoes" element={<Home />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/graficos" element={<Home />} />
-        </Routes>
-      </div>
-      <Footer />
+      {user ? (
+        <>
+          <Header />
+          <Sidebar />
+          <div style={{ marginLeft: '200px', padding: '2rem' }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/filtros" element={<Home />} />
+              <Route path="/tickets" element={<Tickets />} />
+              <Route path="/configuracoes" element={<Home />} />
+              <Route path="/chat" element={<Chat />} />
+              <Route path="/graficos" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </>
+      ) : (
+        <Login />
+      )}
     </>
   );
 }
--- projeto-final/src/Chat/Chat.jsx
+++ projeto-final/src/Chat/Chat.jsx
@@ -1,6 +1,6 @@
 import ChatSideBar from "./ChatAlive/ChatSideBar/ChatSideBar";
 import ChatAlive from "./ChatAlive/ChatAlive";
-import Login from "./Login/Login"
+// import Login from "./Login/Login"
 import { auth } from "../Firebase/firebase";
 import "./Chat.css";
 
@@ -26,23 +26,15 @@ import { useState } from "react";
 
 function Chat(){
 
-  
-  const [user] = useAuthState(auth);
   const [ticketSelecionado, setTicketSelecionado] = useState(null)
 
   return(
     <div>
         <div className="ChatContainer">
-          {user? (
-            <>
-            <ChatSideBar setTicketSelecionado={ setTicketSelecionado } />
-            {ticketSelecionado && <ChatAlive ticketId={ticketSelecionado} />}
-            </>
-            )
-            : (
-              <Login />
-              )
-            }
+          <>
+          <ChatSideBar setTicketSelecionado={ setTicketSelecionado } />
+          {ticketSelecionado && <ChatAlive ticketId={ticketSelecionado} />}
+          </>
         </div>
     </div>
   );
